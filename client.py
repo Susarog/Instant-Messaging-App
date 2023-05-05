72e4a97c61a7df69cad25db238e335c0a6a982b8
Ryan Hang <ryanhang92802@gmail.com>
2023-05-05 01:40:01 -0700

    Fixed TCP data overlap. It was just a variable

--- client.py
+++ client.py
@@ -2,59 +2,35 @@ import socket
 import sys
 import threading
 import errno
-import struct
+from util import *
 
+FORMAT = 'utf-8'
 HOST = socket.gethostname()  
 PORT = int(sys.argv[1])
 USERNAME = sys.argv[2]
-thread = -1 
-
-def send_msg(sock, msg):
-    # Prefix each message with a 4-byte length (network byte order)
-    msg = struct.pack('>I', len(msg)) + msg
-    sock.sendall(msg)
-
-def recv_msg(sock):
-    # Read message length and unpack it into an integer
-    raw_msglen = recvall(sock, 4)
-    if not raw_msglen:
-        return None
-    msglen = struct.unpack('>I', raw_msglen)[0]
-    # Read the message data
-    return recvall(sock, msglen)
-
-def recvall(sock, n):
-    # Helper function to recv n bytes or return None if EOF is hit
-    data = bytearray()
-    while len(data) < n:
-        packet = sock.recv(n - len(data))
-        if not packet:
-            return None
-        data.extend(packet)
-    print(data)
-    return data
 
 def receive_messages(socket):
     while True:
         data = recv_msg(socket)
         if data == None:
-            break;
-        print(f"Message received: {data.decode()}\n""Please enter a command (BM: Broadcast Messaging, EX: Exit): ")
+            break
+        print(f"Message received: {data.decode(FORMAT)}\n""Please enter a command (BM: Broadcast Messaging, EX: Exit): ")
+
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        send_msg(s,USERNAME.encode())
+        send_msg(s,USERNAME.encode(FORMAT))
         thread = threading.Thread(target=receive_messages, args=(s,),daemon=True)
         thread.start()
         while True:
             client_input = input("Please enter a command (BM: Broadcast Messaging, EX: Exit):\n")
             if client_input == "EX":
-                data_message = (client_input + ' ' + USERNAME).encode();
+                data_message = (client_input + ' ' + USERNAME).encode(FORMAT)
                 send_msg(s,data_message)
                 break
             elif client_input == "BM":
                 msg = input("Enter Public Message:\n")
-                data_message = (client_input + ' '+ USERNAME + ' ' + msg).encode(); 
+                data_message = (client_input + ' '+ USERNAME + ' ' + msg).encode(FORMAT) 
                 send_msg(s,data_message)
             else:
                 print("Incorrect command, Please Try Again")
--- server.py
+++ server.py
@@ -1,63 +1,38 @@
 import sys
 import socket
 import threading
-import struct
+from util import *
 
-HOST = socket.gethostname()  
+FORMAT = 'utf-8'
+HOST = socket.gethostname() 
 PORT = int(sys.argv[1])
 CLIENTS = dict()
 lock = threading.Lock()
 threads = list()
 
-def send_msg(sock, msg):
-    # Prefix each message with a 4-byte length (network byte order)
-    msg = struct.pack('>I', len(msg)) + msg
-    sock.sendall(msg)
-
-def recv_msg(sock):
-    # Read message length and unpack it into an integer
-    raw_msglen = recvall(sock, 4)
-    if not raw_msglen:
-        return None
-    msglen = struct.unpack('>I', raw_msglen)[0]
-    # Read the message data
-    return recvall(sock, msglen)
-
-def recvall(sock, n):
-    # Helper function to recv n bytes or return None if EOF is hit
-    data = bytearray()
-    while len(data) < n:
-        packet = sock.recv(n - len(data))
-        if not packet:
-            return None
-        data.extend(packet)
-    return data
-
 def thread_callback(conn, addr):
     with conn:
         while True:
             data = recv_msg(conn)
-            if not data or data == None: break
-            (command, username, *msg) = data.decode().split()
+            if data == None: break
+            (command, username, *msg) = data.decode(FORMAT).split()
             if command == "EX":
-                lock.acquire()
-                del CLIENTS[username]
-                lock.release()
-                print(f"Connection closed by {addr}")
+                with lock:
+                    del CLIENTS[username]
                 break
             elif command == "BM":
                 msg = ' '.join([str(word) for word in msg])
-                print(msg)
-                lock.acquire()
-                for _, (client, conn) in enumerate(CLIENTS.items()):
-                    if (not client == username):
-                        send_msg(conn, msg.encode())
-                lock.release()
+                with lock:
+                    for _, (client, client_conn) in enumerate(CLIENTS.items()):
+                        if (not client == username):
+                            send_msg(client_conn, msg.encode(FORMAT))
             else:
                 print(data)
+        print(f"Connection closed by {addr}")
 
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
         print("Waiting for connection...")
@@ -66,10 +41,9 @@ try:
             data = recv_msg(conn)
             if not data:
                 break
-            lock.acquire()
-            CLIENTS[data.decode()] = conn
-            lock.release()
-            current_thread = threading.Thread(target=thread_callback, args=(conn,addr, ),daemon=True)
+            with lock:
+                CLIENTS[data.decode(FORMAT)] = conn
+            current_thread = threading.Thread(target=thread_callback, args=(conn, addr ))
             threads.append(current_thread)
             current_thread.start()
 except KeyboardInterrupt:
